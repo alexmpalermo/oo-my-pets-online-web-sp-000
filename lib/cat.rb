44e9f1e63474abc33c551c1604741c3ef3e43cda
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-07 21:51:00 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,18 +2,15 @@ class Cat
   attr_accessor :mood
   attr_reader :name
   
-  def initialize(name) 
+  def initialize(name, mood) 
     @name = name
+    @mood = "nervous"
   end 
   
   def name=(name)
     @name = name
    raise NoMethodError if !name.nil?
   end 
-  
-  def mood
-    Owner.play_with_cats
-end
 
 
 
